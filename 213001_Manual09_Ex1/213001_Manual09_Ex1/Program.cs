8874b0a9be9dab67f16ddb4494d34f04a5bd964c
Zahra Mujtaba <213001@std.aumc.edu.pk>
2023-12-04 11:02:50 +0500

    Lab10

---
+++ 213001_Manual09_Ex1/213001_Manual09_Ex1/Program.cs
@@ -0,0 +1,29 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+class IntroToLINQ
+{
+    public static IEnumerable<int>? numQuery { get; private set; }
+
+    static void Main(string[] args)
+    {
+        int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
+
+       
+    numQuery =  from num in
+    numbers
+    where
+    (num % 2) == 0
+    select num;
+        // 3. Query execution.
+        foreach
+        (int num in numQuery)
+        {
+            Console.Write("{0,1} ", num);
+            Console.WriteLine("\n ");
+        }
+        foreach (int num in numQuery)
+        {
+            Console.Write("{0,1} ", num);
+        }
+    }
+}
---
+++ 213001_Manual09_Ex3/213001_Manual09_Ex3/Program.cs
@@ -0,0 +1,59 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Pet
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    class Concat
+    {
+        static void Main(string[] args)
+        {
+
+            Pet[] cats = GetCats();
+            Pet[] dogs = GetDogs();
+
+            IEnumerable<string> query = cats.Select(cats  => cats.Name).Concat(dogs.Select(dog =>dog.Name));
+
+            foreach (var e in query)
+            {
+                Console.WriteLine("{0} ", e);
+            }
+
+            Console.WriteLine("\nPress any key to continue.");
+            Console.ReadKey();
+
+}
+
+        static Pet[] GetCats()
+        {
+            Pet[] cats = {
+
+                new Pet { Name = "Barley", Age = 8 },
+                new Pet { Name = "Boots", Age = 4 },
+                new Pet { Name = "Whiskers", Age = 1 } 
+                         };
+            return cats;
+        }
+
+        static Pet[] GetDogs()
+        {
+            Pet[] dogs = { 
+            new Pet { Name = "Bounder", Age = 3 },
+            new Pet { Name = "Snoopy", Age = 14 },
+            new Pet { Name = "Fido", Age = 9 } };
+
+            return dogs;
+        }
+    }
+
+}
